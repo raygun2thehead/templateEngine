8b6fd49432ee7b5b8db5d828b570ce278058a611
Steven Schaab <stevenes83@gmail.com>
2020-09-14 13:47:05 -0500

    final push

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# templateEngine
+# Template Engine
+
+## Description 
+ Generates a README.md file using the command line 
+ ### Images
+ ![CLI after filled out](./images/one.png)
+ ![team.html afterwards](./images/two.png)
+ ### Screencastify
+ [Walk through video](https://drive.google.com/file/d/1i74kkikc7EnLuMaEtT8t0rra03BF-uDl/view?usp=sharing)
+## Table of contents 
+ - [Installation](#installation) 
+ - [Usage](#usage) 
+ - [contributing](#contributing) 
+ - [Tests](#tests) 
+## Installation 
+ Use this app as a template to putting together different combinations of objects that belong to a subclass of a class.
+## Usage 
+ Once project is installed, run node.js in the command line and follow the prompts. The app will overwrite team.html in the output folder.
+## License 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+## Contributing 
+ Steven Schaab 
+## Tests 
+ N/A 
+## Questions 
+ - [Github](https://github.com/Raygun2thehead) 
+ - [Email](mailto:stevenes83@gmail.com)
--- app.js
+++ app.js
@@ -9,138 +9,144 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { format } = require("path");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-const employeeQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Employee's Name:",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "ID number:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Email Address:",
-    },
-    {
-        type: "list",
-        name: "employeeType",
-        message: "Type of Employee",
-        choices: [
-            "Manager",
-            "Intern",
-            "Engineer"
-        ]
-    }
-];
-
-const managerQuestion = [
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "Office Number:"
-    }
-];
-
-const internQuestion = [
-    {
-        type: "input",
-        name: "school",
-        message: "Name of School:"
-    }
-];
-
-const engineerQuestion = [
-    {
-        type: "input",
-        name: "githubUser",
-        message: "Github UserName:"
-    }
-];
-const allEmployeeData = [];
-
+//asynchonos function that asks questions on cli
 async function askQuestions() {
+    //just learned the try/catch thing today. need to read more on it
+    try {
+        const allEmployeeData = [];
 
-    const employeeAnswers = await inquirer.prompt(employeeQuestions);
+        // while loop that doesnt stop until you answer 'no' to final question
+        var moreEmployees = true
 
-    switch (employeeAnswers.employeeType) {
-        case "Manager": {
-            const managerAnswers = await inquirer.prompt(managerQuestion);
-            employeeAnswers.newAnswer = managerAnswers;
-            break;
-        }
-        case "Intern": {
-            const internAnswers = await inquirer.prompt(internQuestion);
-            employeeAnswers.newAnswer = internAnswers;
-            break;
-        }
-        case "Engineer": {
-            const engineerAnswers = await inquirer.prompt(engineerQuestion);
-            employeeAnswers.newAnswer = engineerAnswers;
-            break;
-        }
-    }
-
-    allEmployeeData.push(employeeAnswers)
-
-    const moreEmployees = await inquirer.prompt([{
-        type: "confirm",
-        name: "another",
-        message: "Add another employee?"
-    }])
-    if (moreEmployees.another) {
-        askQuestions();
-    }
-    await allEmployeeData.forEach(element => {
-        formattedEmpObject = []
-        const name = element.name;
-        const id = element.id;
-        const email = element.email;
-        const employeeType = element.employeeType;
-        switch (employeeType) {
-            case "Manager": {
-                const officeNumber = element.newAnswer.officeNumber;
-                const manager = new Manager(name, id, email, officeNumber);
-                formattedEmpObject.push(manager);
-                break;
+        while (moreEmployees) {
+            // basic employee questions
+            const employeeAnswers = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "Employee's Name:",
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "ID number:",
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Email Address:",
+                },
+                {
+                    type: "list",
+                    name: "employeeType",
+                    message: "Type of Employee",
+                    choices: [
+                        "Manager",
+                        "Intern",
+                        "Engineer"
+                    ]
+                }
+            ]);
+            //employee questions based on employeeType
+            switch (employeeAnswers.employeeType) {
+                case "Manager": {
+                    const managerAnswers = await inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "officeNumber",
+                            message: "Office Number:"
+                        }
+                    ]);
+                    employeeAnswers.newAnswer = managerAnswers;
+                    break;
+                }
+                case "Intern": {
+                    const internAnswers = await inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "school",
+                            message: "Name of School:"
+                        }
+                    ]);
+                    employeeAnswers.newAnswer = internAnswers;
+                    break;
+                }
+                case "Engineer": {
+                    const engineerAnswers = await inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "githubUser",
+                            message: "Github UserName:"
+                        }
+                    ]);
+                    employeeAnswers.newAnswer = engineerAnswers;
+                    break;
+                }
             }
-            case "Intern": {
-                const school = element.newAnswer.school;
-                const intern = new Intern(name, id, email, school);
-                formattedEmpObject.push(intern);
-                break;
-            }
-            case "Engineer": {
-                const github = element.newAnswer.github;
-                const engineer = new Engineer(name, id, email, github);
-                formattedEmpObject.push(engineer);
-                break;
-            }
+            allEmployeeData.push(employeeAnswers)
+
+            // more employees prompt that ends or continues while loop
+            const moreEmployeesQ = await inquirer.prompt([{
+                type: "confirm",
+                name: "another",
+                message: "Add another employee?"
+            }])
+
+            moreEmployees = moreEmployeesQ.another
         }
-        return (formattedEmpObject);
-    });
 
-
-        const output = await render(formattedEmpObject);
-    
-        fs.writeFile(outputPath, output, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            else {
-                console.log("Successfully wrote the team.html file!");
+        // foreach loop that goes through the array and formats it with the proper imported function, creating new sub classes
+        const formattedEmpArr = []
+        
+        allEmployeeData.forEach(element => {
+            const name = element.name;
+            const id = element.id;
+            const email = element.email;
+            const employeeType = element.employeeType;
+            switch (employeeType) {
+                case "Manager": {
+                    const officeNumber = element.newAnswer.officeNumber;
+                    const manager = new Manager(name, id, email, officeNumber);
+                    formattedEmpArr.push(manager);
+                    break;
+                }
+                case "Intern": {
+                    const school = element.newAnswer.school;
+                    const intern = new Intern(name, id, email, school);
+                    formattedEmpArr.push(intern);
+                    break;
+                }
+                case "Engineer": {
+                    const githubUser = element.newAnswer.githubUser;
+                    const engineer = new Engineer(name, id, email, githubUser);
+                    formattedEmpArr.push(engineer);
+                    break;
+                }
             }
         });
-    
+        return (formattedEmpArr);
+    }
+    catch (err) {
+        // i need this and it set me back hours figuring it out
+        console.log(err);
+    }
 }
-askQuestions()
 
+// function that starts the app and eventually builds the team page
+async function teamBuild() {
+    const formattedEmpArr = await askQuestions();
+    const output = await render(formattedEmpArr);
 
+    fs.writeFile(outputPath, output, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        else {
+            console.log("Successfully wrote the team.html file!");
+        }
+    });
+}
 
+teamBuild();
